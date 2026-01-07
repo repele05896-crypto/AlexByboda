3d8fbe906044c59230ac3465d56aba8ec3182278
repele05896-crypto <repele05896@gmail.com>
2026-01-07 19:57:29 +0200

    Update logger.py

--- AlexaMusic/utils/logger.py
+++ AlexaMusic/utils/logger.py
@@ -19,18 +19,18 @@ from AlexaMusic import app
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} 𝖯𝗅𝖺𝗒 𝖫𝗈𝗀</b>
+<b>سـجـل تـشـغـيـل {app.mention}</b>
 
-<b>𝖢𝗁𝖺𝗍 𝖨𝖣 :</b> <code>{message.chat.id}</code>
-<b>𝖢𝗁𝖺𝗍 𝖭𝖺𝗆𝖾 :</b> {message.chat.title}
-<b>𝖢𝗁𝖺𝗍 𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.chat.username}
+<b>آيـدي الـمـجـمـوعـة :</b> <code>{message.chat.id}</code>
+<b>اسـم الـمـجـمـوعـة :</b> {message.chat.title}
+<b>يـوزر الـمـجـمـوعـة :</b> @{message.chat.username}
 
-<b>𝖴𝗌𝖾𝗋 𝖨𝖣 :</b> <code>{message.from_user.id}</code>
-<b>𝖴𝗌𝖾𝗋 𝖭𝖺𝗆𝖾 :</b> {message.from_user.mention}
-<b>𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.from_user.username}
+<b>آيـدي الـمـشـغـل :</b> <code>{message.from_user.id}</code>
+<b>اسـم الـمـشـغـل :</b> {message.from_user.mention}
+<b>يـوزر الـمـشـغـل :</b> @{message.from_user.username}
 
-<b>𝖰𝗎𝖾𝗋𝗒 :</b> {message.text.split(None, 1)[1]}
-<b>𝖲𝗍𝗋𝖾𝖺𝗆-𝖳𝗒𝗉𝖾 :</b> {streamtype}"""
+<b>الـمـطـلـوب :</b> {message.text.split(None, 1)[1]}
+<b>نـوع الـتـشـغـيـل :</b> {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
