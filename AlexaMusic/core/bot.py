387abd550fcd701f9a0398e91c3b9d8e1507603d
repele05896-crypto <repele05896@gmail.com>
2026-01-07 21:09:25 +0200

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -27,7 +27,7 @@ class AlexaBot(Client):
             bot_token=config.BOT_TOKEN,
             max_concurrent_transmissions=5,
         )
-        LOGGER(__name__).info("Starting Bot...")
+        LOGGER(__name__).info("جاري تهيئة ملفات البوت وبدء التشغيل...")
 
     async def start(self):
         await super().start()
@@ -37,19 +37,20 @@ class AlexaBot(Client):
         self.mention = get_me.mention
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
+                config.LOG_GROUP_ID,
+                "🧚 أهلاً بك سيدي المطور، لقد تم تشغيل البوت الخاص بك بنجاح وهو الآن يعمل بكامل طاقته في السيرفر. نحن الآن في انتظار انضمام الحساب المساعد للبدء في تشغيل الموسيقى."
             )
         except Exception:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "🥀 عذراً، فشل البوت في الوصول إلى مجموعة السجل الخاصة بك. يرجى التأكد من أنك قمت بإضافة البوت إلى القناة أو المجموعة المخصصة للسجل، وتأكد أيضاً من رفعه مشرفاً (Admin) بصلاحيات كاملة."
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            LOGGER(__name__).error("🥀 عذراً، يرجى رفع البوت مشرفاً (Admin) في مجموعة السجل (Logger Group) حتى يتمكن من إرسال التقارير والعمل بشكل صحيح.")
             sys.exit()
         if get_me.last_name:
             self.name = f"{get_me.first_name} {get_me.last_name}"
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"تم بدء تشغيل البوت بنجاح تحت اسم: {self.name}")
